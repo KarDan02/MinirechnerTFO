890b8ee6e18dc79c8c647f59484aff11b3ca54ab
KarDan02 <02kardan@rgtfo-me.it>
2020-04-23 13:44:49 +0200

    Basis für den Rechner programmiert: Zahleneingabe

---
+++ MinirechnerTFO_KarasaniDaniel/MinirechnerTFO_KarasaniDaniel/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MinirechnerTFO_KarasaniDaniel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Zahl1: ");
+            int Zahl1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Zahl2: ");
+            int Zahl2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\n1.Zahl: "+Zahl1+"\n2.Zahl: "+Zahl2);
+
+            Console.ReadKey();
+        }
+    }
+}
